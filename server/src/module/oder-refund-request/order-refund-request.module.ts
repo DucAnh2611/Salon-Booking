f5616b70e4a2d69679ca40b2d233d62a67fad7e4
ADMIN\Admin <ducanhmc24@gmail.com>
2024-10-10 17:54:45 +0700

    Forget add entity to module

--- server/src/module/oder-refund-request/order-refund-request.module.ts
+++ server/src/module/oder-refund-request/order-refund-request.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { BankModule } from '../bank/bank.module';
+import { OrderEntity } from '../order-base/entity/order-base.entity';
 import { OrderBaseModule } from '../order-base/order-base.module';
 import { OrderRefundStateModule } from '../order-refund-state/order-refund-state.module';
 import { OrderRefundRequestEntity } from './entity/order-refund-request.entity';
@@ -8,7 +9,7 @@ import { OrderRefundRequestService } from './order-refund-request.service';
 
 @Module({
     imports: [
-        TypeOrmModule.forFeature([OrderRefundRequestEntity]),
+        TypeOrmModule.forFeature([OrderRefundRequestEntity, OrderEntity]),
         OrderBaseModule,
         OrderRefundStateModule,
         BankModule,
